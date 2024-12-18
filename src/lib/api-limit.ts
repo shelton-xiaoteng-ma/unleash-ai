2ddaee37783d8a10ae2d3bf545b53a22306927b0
maxiaoteng <jizhuwo00@hotmail.com>
2024-12-18 18:13:17 +0800

    ApiLimit api

---
+++ src/lib/api-limit.ts
@@ -0,0 +1,42 @@
+import { MAX_FREE_COUNTS } from "@/constants";
+import { prismadb } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export const increaseApiLimit = async () => {
+  const { userId } = await auth();
+  if (!userId) return;
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { userId },
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId },
+      data: { count: userApiLimit.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: {
+        userId,
+        count: 1,
+      },
+    });
+  }
+};
+
+export const checkApiLimit = async () => {
+  const { userId } = await auth();
+
+  if (!userId) return false;
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { userId },
+  });
+  console.log(userApiLimit);
+  if (userApiLimit) {
+    return userApiLimit.count < MAX_FREE_COUNTS;
+  } else {
+    return true;
+  }
+};
---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const prismadb = new PrismaClient({ adapter });
